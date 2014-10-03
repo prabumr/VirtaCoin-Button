0b9075cfa0c45ab4d5aaecc1439b09c0ebd20073
prabumr <prabu8510@yahoo.com>
2014-10-03 12:07:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,559 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" media="screen" href="coinwidget.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="coin.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="coinwidget.js"></script>
+	<!--[if (IE 7) | (IE 8)]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<script>
+			$(function(){
+				$('img.gear').remove();
+			});
+		</script>
+	<![endif]-->
+	
+</head>
+<body>	
+<section>
+		<article class="primary">
+		<h1>Add a VirtaCoin donation button, buy button and some buttons to your webpage and start getting coins today!</h1>
+		<p>It's simple to use, clean cut, and loaded with customizable features.</p><br/>
+		<p><b>Here is what it looks like, go ahead and give it a try:</b><br/>
+		<span id="preview_holder"><span id="preview">
+			<script src="coin.js"></script>
+<BR>
+			</article>
+<BR>
+<table width="960px" align="center">
+<tr>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>
+CoinWidgetCom.go({
+	wallet_address: "1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP"
+	, currency: "virtacoin"
+	, button: "bi"
+	, linked: "no"
+	, counter: "hide"
+	, alignment: "al"
+	, qrcode: true
+	, auto_show: false
+	, lbl_button: "Button 9"
+	, lbl_address: "My VirtaCoin Address:"
+	, lbl_count: "donations"
+	, lbl_amount: "VTA"
+});
+</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>
+CoinWidgetCom.go({
+	wallet_address: "1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP"
+	, currency: "virtacoin"
+	, button: "bj"
+	, linked: "no"
+	, counter: "hide"
+	, alignment: "al"
+	, qrcode: true
+	, auto_show: false
+	, lbl_button: "Button 10"
+	, lbl_address: "My VirtaCoin Address:"
+	, lbl_count: "donations"
+	, lbl_amount: "VTA"
+});
+</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>
+CoinWidgetCom.go({
+	wallet_address: "1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP"
+	, currency: "virtacoin"
+	, button: "bk"
+	, linked: "no"
+	, counter: "hide"
+	, alignment: "al"
+	, qrcode: true
+	, auto_show: false
+	, lbl_button: "Button 11"
+	, lbl_address: "My VirtaCoin Address:"
+	, lbl_count: "donations"
+	, lbl_amount: "VTA"
+});
+</script>
+</div>
+</td>
+</tr>
+<tr>
+<td width="320px" style="height: 100px;">
+<BR>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bd'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Button 4'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<BR><BR>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'be'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Button 5'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<BR>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bf'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Button 6'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+<tr>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bg'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Button 7'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bh'
+                     , linked        : 'yes'
+				, lbl_button 	: 'VirtaCoin Fast Rocket Button 8'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+<td width="320px" style="height: 100px;">
+<div style="margin-bottom: 15px;"></div>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bl'
+                     , linked        : 'yes'
+				, lbl_button 	: 'VirtaCoin Button 12'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+</tr>
+<tr>
+<td width="320px" style="height: 100px;">
+<div style="margin-bottom: 30px;"></div>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'ba'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Donate VirtaCoin Button 1'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+<div style="margin-bottom: 50px;"></div>
+</td>
+<td width="320px" style="height: 100px;">
+<div style="margin-bottom: 30px;"></div>
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bb'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Donate VirtaCoin Button 2'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+<div style="margin-bottom: 50px;"></div>
+</td>
+<td width="320px" style="height: 100px;">
+<div align="center">
+<script>CoinWidgetCom.go({
+				wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+				, currency		: 'virtacoin'
+				, counter 		: 'amount'
+				, alignment 	: 'al'
+                     , button        : 'bc'
+                     , linked        : 'yes'
+				, lbl_button 	: 'Donate VirtaCoin Button 3'
+				, lbl_address	: 'My VirtaCoin Address:'
+				, lbl_count 	: 'donations'
+				, lbl_amount	: 'VTA'
+				, qrcode 		: true
+				, decimals 		: 2
+			});
+			</script>
+</div>
+</td>
+</tr>
+</table>
+
+<BR>
+	<article id="get_it">
+		<img style="position:absolute;top:15px;left:20px;" src="images/icon_75_gear.png" />
+		<div style="margin-left:90px;">
+			<h1>Get the coin widget right now:</h1>
+			<p>Use the wizard to quickly create a coin widget button for your site right now.</p><BR>
+<div id="go_buttons">			
+<button onclick="wizard.start();" style="box-shadow:inset 1px 1px 0px #76b5f2;padding:8px;"><img src="icon_virtacoin.png" />Create a Coin Widget now!</button> 			
+		</div><BR>
+<small style="margin-top:4px;display:block;font-size:10px;"><p><B>Not: Button codes:   , button: "ba" -->1st button   , button: "bb" -->2nd button   , button: "bc" -->3rd button  , button: "bd" -->4th button , button: "be" -->5th button  , button: "bf" -->6th button  , button: "bg" -->7th button  , button: "bh" -->8th button , button: "bi" -->9th button  , button: "bj" -->10th button , button: "bk" -->11th button , button: "bl" -->12th button<BR><BR>
+Only Button into web page : counter 		: 'hide' ,  linked        : 'no' , lbl_button 	: '' </B></p></small>
+			<br/>
+		</div>
+</article>
+<BR>
+	<article>
+		<img style="position:absolute;top:15px;left:20px;" src="images/icon_75_settings.png" />
+		<div style="margin-left:90px;">
+			<h1>Manual Configuration</h1>
+			<p>Add any of these options to the launcher code to customize this widget.</p>
+		</div>
+		<br/><br/>
+		<table cellspacing="0" cellpadding="0" border="0">
+		<thead>
+			<tr><th width="100">Option</th>
+			<th width="120">Default Value</th>
+			<th width="120">Acceptable Values</th>
+			<th class="col_desc">Description</th>
+		</tr></thead>
+		<tbody>
+			<tr>
+				<td><b>currency</b></td>
+				<td>virtacoin</td>
+				<td>virtacoin</td>
+				<td class="col_desc">This widget supports VirtaCoin</td>
+			</tr>
+			
+			<tr>
+				<td><b>counter</b></td>
+				<td>count</td>
+				<td>count<br>amount<br>hide</td>
+				<td class="col_desc">Toggle what is shown in the counter next to the main button.<br><div style="padding:0px 5px;margin:5px;background:#FCF9ED;border:1px solid #f3f0ea;"><b>count</b> will show the total number of transactions.<br><b>amount</b> will show the total amount received (in VTA).<br><b>hide</b> will hide the counter as well as hide the transaction count and total amount received within the window.</div></td>
+			</tr>
+<tr>
+				<td><b>button</b></td>
+				<td>ba</td>
+				<td>ba<br>bb<br>bc<br>bd<br>be<br>bf<br>bg<br>bh<br>bi<br>bj<br>bk<br>bl</td>
+				<td class="col_desc">Choose what type of button you need.<br><div style="padding:0px 5px;margin:5px;background:#FCF9ED;border:1px solid #f3f0ea;"><b>button: "ba"</b> On this code type button 1.<br><b>button: "bb"</b> On this code type button 2.<br><b>
+button: "bc"</b> On this code type button3.<br><b>
+button: "bd"</b> On this code type button 4.<br><b>
+button: "be"</b> On this code type button 5.<br><b>
+button: "bf"</b> On this code type button 6.<br><b>
+button: "bg"</b> On this code type button 7.
+<br><b>
+button: "bh"</b> On this code type button 8.
+<br><b>
+button: "bi"</b> On this code type button 9.
+<br><b>
+button: "bj"</b> On this code type button 10.
+<br><b>
+button: "bk"</b> On this code type button 11.
+<br><b>
+button: "bl"</b> On this code type button 12.
+</div></td>
+			</tr>
+<tr>
+				<td><b>linked</b></td>
+				<td>yes</td>
+				<td>yes<br>no</td>
+				<td class="col_desc">What type of button you need linked or not.<br><div style="padding:0px 5px;margin:5px;background:#FCF9ED;border:1px solid #f3f0ea;"><b>yes:</b> The button is linked popup to show info.<br><b>no:</b> no link added to the button, you use only button into your web page.<br></div>Button 1- link value default yes only, Button 2 and Button 3 - There is no link.</td>
+			</tr>
+
+			<tr>
+				<td><b>qrcode</b></td>
+				<td>true</td>
+				<td>true<br>false</td>
+				<td class="col_desc">Set to true if you want to show the QR code generator that appears at the bottom left of the window.<br/>Set to false to hide the QR code icon.</td>
+			</tr>
+			<tr>
+				<td><b>auto_show</b></td>
+				<td>false</td>
+				<td>true<br>false</td>
+				<td class="col_desc">Set to true if you want the window to auto appear as soon as the counter finishes loading.</td>
+			</tr>
+			<tr>
+				<td><b>decimals</b></td>
+				<td>4</td>
+				<td>0-10</td>
+				<td class="col_desc">Adjust the number of decimals shown on the amount received statistic.</td>
+			</tr>
+
+			<tr>
+				<td><b>lbl_button</b></td>
+				<td>Donate</td>
+				<td><em>(anything)</em></td>
+				<td class="col_desc">Change the text of the label on the main button.</td>
+			</tr>
+			<tr>
+				<td><b>lbl_address</b></td>
+				<td>My VirtaCoin Address:</td>
+				<td><em>(anything)</em></td>
+				<td class="col_desc">The text that appears above your virtacoin wallet address within the window.</td>
+			</tr>
+			<tr>
+				<td><b>lbl_count</b></td>
+				<td>donations</td>
+				<td><em>(anything)</em></td>
+				<td class="col_desc">The text that appears in the window under the total number of transactions.</td>
+			</tr>
+			<tr>
+				<td><b>lbl_amount</b></td>
+				<td>VTA</td>
+				<td><em>(anything)</em></td>
+				<td class="col_desc">The text that appears in the window under the total amount of VirtaCoins received.</td>
+			</tr>
+			<tr>
+				<td><b>onShow</b></td>
+				<td>null</td>
+				<td>function</td>
+				<td class="col_desc">Execute a function when the window opens.</td>
+			</tr>
+			<tr>
+				<td><b>onHide</b></td>
+				<td>null</td>
+				<td>function</td>
+				<td class="col_desc">Execute a function when the window closes.</td>
+			</tr>
+		</tbody>
+	</table>
+	<small style="margin-top:4px;display:block;font-size:10px;"><p><B>Not sure how to work these options into the code?</B> <a style="color:#666;" href="javascript:;" onclick="$('.example').show();"><B>This example</b></a> <b>might help.</B></p></small>
+
+	<div class="example" style="display: none;"><br/>
+<pre>
+&lt;script src="http://localhost/coin.js"&gt;&lt;/script&gt;
+&lt;script&gt;
+	CoinWidgetCom.go({
+		wallet_address		: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP',
+		currency 		: 'virtacoin',
+		counter			: 'amount',
+                button			: 'ba',
+                linked			: 'yes',
+		lbl_button 		: 'TIP ME',	
+		lbl_count		: 'payments',
+		lbl_amount 		: 'coins',
+		lbl_address		: 'Please send virtacoin tips to:',
+		qrcode			: false,
+		decimals		: 0,
+		onShow 			: function(){
+			alert('window opened!');
+		},
+		onHide 			: function(){
+			alert('window closed!');
+		}
+	});
+&lt;/script&gt;
+</pre>
+	<br/><small><b>Note:</b> Make sure your last option does not have a trailing comma.</small>
+	</div>
+	</article>
+
+</section>
+
+<div id="fade"></div>
+
+
+<div id="code">
+	<button style="position:absolute;right:5px;top:5px;font-size:11px;padding:3px 6px;z-index:9;font-weight:bold;" onclick="wizard.code.close();" title="Close Window" class="grey">X</button>
+	
+	<div align="center">
+		<h1>Paste the code below into your website to install your widget:</h1>
+	
+		<textarea onclick="this.select();"></textarea>
+		
+		<button style="width:150px;text-align:center;" onclick="wizard.code.close_all();">Done, Close Wizard</button>
+		<button style="width:150px;text-align:center;" class="grey" onclick="wizard.code.close();">Go back and Edit</button><br/><br/>
+</div>
+	</div>
+</div>
+
+
+<div id="wizard">
+	<button style="position:absolute;right:5px;top:5px;font-size:11px;padding:3px 6px;z-index:9;font-weight:bold;" onclick="wizard.close();" title="Close Window" class="grey">X</button>
+	<div class="loading">
+		<img src="images/icon_72_gear.png" />
+	</div>
+	<article>
+		<aside>
+			<b>select an option:</b>
+			<ul>
+				<li data-page="currency" class="sel">Wallet:<b>VirtaCoin</b></li>
+				<li data-page="counter">Counter:<b>Transaction Count</b></li>
+				<li data-page="text">Text:<b>Default</b></li>
+				<li data-page="alignment">Window Alignment:<b>Below, Left</b></li>
+				<li data-page="qrcode">QR Code:<b>On</b></li>
+				<li data-page="autoshow">Auto Show:<b>Off</b></li>
+			</ul>
+			
+		</aside>
+		<section class="preview">
+			<div>
+				<script>
+				CoinWidgetCom.go({
+					wallet_address 	: '1PFKR5fEcjpfPCBDASVXH8oatNMsy7cKMP'
+					, currency		: 'virtacoin'
+					, counter 		: 'count'
+					, alignment 	: 'bl'
+                           , button        : 'ba'
+					, lbl_button 	: 'Donate'
+					, lbl_address	: 'My VirtaCoin Address:'
+					, lbl_count 	: 'donations'
+					, lbl_amount	: 'VTA'
+					, qrcode 		: true
+                           , button: false
+				});
+				</script> 
+			</div>
+			<button onclick="wizard.get_code();">Finished? Click here to get the code!</button>
+		</section>
+		<section class="step">
+			<div data-page="currency">
+				<h2>VirtaCoin cryptocurrency button.</h2>
+				<button data-sidebar="VirtaCoin" data-currency="vta"><img src="images/icon_virtacoin.png" />VirtaCoin</button>
+				<br/>
+				<h2>Enter your wallet address for this widget:</h2>
+				<input type="text" style="width:295px;" name="wallet_address" onclick="$(this).select();" />
+			</div>
+			<div data-page="counter" class="hide">
+				<h2>What should the counter show?</h2>
+				<button data-sidebar="Transaction Count" data-counter="count"><img src="images/icon_16_counter.png" />transaction count</button>
+				<button data-sidebar="Amount Received" data-counter="received" class="grey"><img src="images/icon_16_counter.png" />amount received</button>
+				<button data-sidebar="Hidden" data-counter="hide" class="grey"><img src="images/icon_16_counter.png" />hidden</button>
+				<small style="font-size:11px;display:block;margin-top:8px;border-top:1px solid #ddd;padding-top:8px;">Selecting hidden will hide all visible statistics.</small>
+			</div>			
+			<div data-page="text" class="hide" data-custom="false">
+				<h2>Customize Text</h2>
+				<label>Button Label</label>		<input name="button" type="text" /><br/>
+				<label>Address Label</label>	<input name="address" type="text" /><br/>
+				<label>Count Label</label>		<input name="count" type="text" /><br/>
+				<label>Amount Label</label>		<input name="amount" type="text" /><br/>
+				<a id="text_restore" href="#">Restore Defaults</a>
+			</div>
+			<div data-page="alignment" class="hide">
+				<h2>Window Alignment</h2>
+				<p>How should the window align to the button when opened?</p><br/>
+				<div align="center"> 
+					<table id="alignment" cellspacing="1" cellpadding="0" border="0">
+						<tr>
+							<td data-point="al" class="point"><img src="images/icon_16_check2.png" /></td>
+							<td data-point="ac" class="point"><img src="images/icon_16_check2.png" /></td>
+							<td data-point="ar" class="point"><img src="images/icon_16_check2.png" /></td>
+						</tr>
+						<tr>
+							<td colspan="3" class="mid_point"><span>Below, Left</span></td>
+						</tr>
+						<tr>
+							<td data-point="bl" class="sel point"><img src="images/icon_16_check2.png" /></td>
+							<td data-point="bc" class="point"><img src="images/icon_16_check2.png" /></td>
+							<td data-point="br" class="point"><img src="images/icon_16_check2.png" /></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div data-page="qrcode" class="hide">
+				<h2>QR Code</h2>
+				<p>Do you want this widget to show a QR code of your wallet address?</p><br/>
+				<button data-sidebar="On" data-qrcode="on"><img src="images/icon_16_check.png" />On</button>
+				<button data-sidebar="Off" data-qrcode="off" class="grey"><img src="images/icon_16_cancel.png" />Off</button>
+			</div>
+			<div data-page="autoshow" class="hide">
+				<h2>Auto Show</h2>
+				<p>Do you want this widget to automatically show the window after the counter finishes loading?</p><br/>
+				<button data-sidebar="On" data-autoshow="on" class="grey"><img src="images/icon_16_check.png" />On</button>
+				<button data-sidebar="Off" data-autoshow="off"><img src="images/icon_16_cancel.png" />Off</button>
+			</div>
+		</section>
+	</article>
+</div><BR>
+<div class="page-header"></div>
+
+</body>
+</html>
